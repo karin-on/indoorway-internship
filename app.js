634c81ff6421e38da008dbee9881454904f6f69d
Karina Nowicka <knowicka@gmail.com>
2019-05-07 21:06:01 +0200

    added: amount in stock decreasing, warning

--- app.js
+++ app.js
@@ -6,6 +6,10 @@
  * 1. Show selected stuff description in the #description box.
  * 2. What else can shop have?
  - **Write your ideas here or add these functionalities and write here what you have done.**
+ What I added:
+ a) after adding a product to a cart, its amount in stock decreases;
+ b) it's impossible to add to a cart a product which is out of stock;
+ c) there is a warning beneath a description showing if a product you try to buy is out of stock;
  **/
 
 // TODO: What does it do?
@@ -14,14 +18,14 @@
 var stuffToBuy = [
     {
         name: 'SamPhone X9',
-        amount: 3,
+        amount: 5,
         value: 1250,
         // TODO: Replace \x27 with ' char.
         desc: "It's like a fusion of Iphane and Samsong."
     },
     {
         name: 'TV MX3000 300"',
-        amount: 1,
+        amount: 3,
         value: 3000,
         desc: 'Biggest TV you can imagine.'
     }
@@ -41,6 +45,7 @@ var shopApp = {
         this.addStuffForm = document.querySelector('#addStuffForm');
         this.cartElement = document.querySelector('#cart');
         this.description = document.querySelector('#description');
+        this.warning = document.querySelector('#warning');
 
         // Bind events
         this.stuffSelect.addEventListener('change', this.onStuffChange);
@@ -54,6 +59,7 @@ var shopApp = {
     },
 
     populateStuffSelect: function populateStuffSelect() {
+        this.stuffSelect.innerHTML = '';
         // Populate with buy options
         for (var i = 0; i < this.stuff.length; i++) {
             var stuffToAdd = this.stuff[i];
@@ -74,16 +80,49 @@ var shopApp = {
         return this;
     },
 
+    isAvailable: function isAvailable(index) {
+        return this.stuff[index].amount > 0;
+    },
+
+    showWarning: function showWarning() {
+        this.warning.innerHTML = "You can't buy this product anymore. It is no longer in stock.";
+        return this;
+    },
+
+    hideWarning: function hideWarning() {
+        this.warning.innerHTML = '';
+        return this;
+    },
+
     addToCart: function addToCart(index) {
+        var index = index ? index : this.stuffSelect.selectedIndex;
+        console.log(index);
+
         var stuff = {
             // TODO: How we call this statement?
             //KN: ternary operator
-            name: index ? this.stuff[index].name : this.stuffSelect.selectedOptions[0].value,
+            // name: index ? this.stuff[index].name : this.stuffSelect.selectedOptions[0].value,
+            name: this.stuff[index].name,
             amount: 1
         };
         // TODO: Other way of adding elements to an array?
         // this.cart[this.cart.length] = stuff;
-        this.cart.push(stuff);
+        //KN: this.cart.push(stuff);
+
+        // this.stuff[index].amount > 0 ? this.cart.push(stuff) : this.warning.innerHTML = "You can't buy this product anymore. It is no longer in stock.";
+        //
+        // //change amount available
+        // this.stuff[index].amount > 0 ? this.stuff[index].amount-- : null;
+        // this.populateStuffSelect();
+
+        if (this.isAvailable(index)) {
+            this.cart.push(stuff);
+            this.stuff[index].amount--;
+            this.populateStuffSelect();
+        } else {
+            this.showWarning();
+        }
+
 
         return this;
     },
@@ -111,17 +150,17 @@ var shopApp = {
      * Events
      **/
     onStuffChange: function onStuffChange() {       //zmiana selecta
-        console.log(this);
-        console.log(shopApp.stuffSelect.selectedIndex);
         // TODO: Update description of thing you just chosen
         shopApp.showDescription(shopApp.stuffSelect.selectedIndex);
+
+        !shopApp.isAvailable(shopApp.stuffSelect.selectedIndex) ?
+            shopApp.showWarning() :
+            shopApp.hideWarning();
     },
 
     // TODO: How we call this expression?
     //KN: fat arrow function
     onStuffAdd: (event) => {        //submit forma
-        console.log(this);
-
         event.preventDefault();
         // TODO: Other way to call shopApp.addToCart()?
         // shopApp.addToCart();
